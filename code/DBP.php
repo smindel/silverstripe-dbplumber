a66aa48c492bbab1b6eff205807c20b65e88416a
Andreas Piening <andreas@silverstripe.com>
2010-06-19 08:48:45 +0000

    MINOR: require permission

--- code/DBP.php
+++ code/DBP.php
@@ -40,13 +40,18 @@ class DBP_Controller extends Controller {
 	protected $instance;
 	protected $model;
 	
-	public function __construct($id) {
+	public function __construct($id = null) {
 		parent::__construct();
 		if(!preg_match('/^DBP_([a-z]+)_Controller$/i', get_class($this), $matches)) throw new Exception(get_class($this) . ' can\'t be instantiated');
 		$this->model = $model = $matches[1];
 		$modelClass = 'DBP_' . $matches[1];
 		if(!preg_match('/^[a-z0-9_\.]*$/i', $id)) throw new Exception('Invalid ' . $model . ' ID "' . $request->Param('ID') . '"');
 		$this->instance = new $modelClass($id);
+	}
+	
+	function init() {
+		parent::init();
+		if(!Permission::check('ADMIN')) return Security::permissionFailure($this);
 	}
 	
 	function show() {
