6dc5edf177e70be87cf5f2644cd89a5463869ccb
Fred Condo <fcondo@quinn.com>
2012-04-24 16:43:43 -0700

    Correct typos, punctuation, and word choice.

--- code/RemoveArtefactsTask.php
+++ code/RemoveArtefactsTask.php
@@ -4,7 +4,7 @@ class RemoveArtefactsTask extends BuildTask {
 
 	protected $title = 'Remove Database Artefacts';
 	
-	protected $description = 'During devolpment of your Silverstripe application you may have deleted a data object class or removed a field from a data object. This leaves obsolete columns and tables in your databse behind. Because these columns or tables may contain data that must not be deleted sapphire doesn\'t delete those automatically. But if you know what you are doing and you are sure that these remains are no longer required, you can run this task to delete them. This is irreversible.';
+	protected $description = 'During development of your SilverStripe application you may have deleted a data object class or removed a field from a data object. This leaves obsolete columns and tables in your database behind. Because these columns or tables may contain data that must not be deleted, sapphire doesn\'t delete those automatically. But if you know what you are doing and you are sure that these remnants are no longer required, you can run this task to delete them. This is irreversible.';
 	
 	function init() {
 		parent::init();
@@ -18,7 +18,7 @@ class RemoveArtefactsTask extends BuildTask {
 		$db = new DBP_Database();
 		$artefacts = $db->Artefacts();
 		if(empty($artefacts)) {
-			echo '<h3>Schema is clean, nothin to drop.</h3>';
+			echo '<h3>Schema is clean; nothing to drop.</h3>';
 		} else {
 			echo '<h3>Dropping:</h3>';
 			echo '<ul>';
